51dffb124dfbaee39ca72613554cdb566ce339d5
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-11-22 19:44:55 -0300

    Requisito 2 concluído, requisito 3 em andamento

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,47 +1,86 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 export default function Table() {
-  const { planets } = useContext(PlanetsContext);
+  const {
+    planets,
+    setFilterName,
+    setFilterNumber,
+  } = useContext(PlanetsContext);
+
+  const [filterNa, setFilterNa] = useState('');
+  const [filterNu, setFilterNu] = useState('');
+
+  useEffect(() => {
+    setFilterName({ name: filterNa });
+  }, [setFilterName, filterNa]);
+
+  useEffect(() => {
+    setFilterNumber({ name: filterNu });
+  }, [setFilterNumber, filterNu]);
+
+  console.log(planets);
 
   return (
-    <table>
-      <thead>
-        <tr className="align-middle">
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {planets.map((planet) => (
-          <tr key={ planet.name }>
-            <td data-testid="planet-name">{ planet.name }</td>
-            <td>{ planet.rotation_period }</td>
-            <td>{ planet.orbital_period }</td>
-            <td>{ planet.diameter }</td>
-            <td>{ planet.climate }</td>
-            <td>{ planet.gravity }</td>
-            <td>{ planet.terrain }</td>
-            <td>{ planet.surface_water }</td>
-            <td>{ planet.population }</td>
-            <td>{ planet.films }</td>
-            <td>{ planet.created }</td>
-            <td>{ planet.edited }</td>
-            <td>{ planet.url }</td>
+    <main>
+      <label htmlFor="filter-name">
+        Search:
+        <input
+          data-testid="name-filter"
+          type="text"
+          name="filter-name"
+          value={ filterNa }
+          onChange={ ({ target: { value } }) => setFilterNa(value) }
+        />
+      </label>
+      <label htmlFor="filter-number">
+        Search:
+        <input
+          data-testid="number-filter"
+          type="text"
+          name="filter-number"
+          value={ filterNu }
+          onChange={ ({ target: { value } }) => setFilterNu(value) }
+        />
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {planets.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 }
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -4,18 +4,39 @@ import fetchAPI from '../services/fetchAPI';
 import PlanetsContext from './PlanetsContext';
 
 export default function PlanetsProvider({ children }) {
+  const [apiData, setApiData] = useState([]);
   const [planets, setPlanets] = useState([]);
+  const [filterName, setFilterName] = useState('');
+  const [filterNumber, setFilterNumber] = useState('');
 
   useEffect(() => {
     async function getApiResult() {
       const apiResult = await fetchAPI();
       setPlanets(apiResult);
+      setApiData(apiResult);
     }
     getApiResult();
   }, []);
 
+  useEffect(() => {
+    const filteredByName = apiData
+      .filter((planet) => planet.name.toLowerCase()
+        .includes(filterName.name.toLowerCase()));
+    setPlanets(filteredByName);
+  }, [filterName, apiData, setPlanets]);
+
+  // useEffect(() => {
+  //   const filteredByNumber = apiData
+  //     .filter((planet) => planet.name.toLowerCase()
+  //       .includes(filteredByNumber.name.toLowerCase()));
+  //   setPlanets(filteredByNumber);
+  // }, [filterNumber, apiData, setPlanets]);
+  console.log(filterNumber);
+
   const allData = {
     planets,
+    setFilterName,
+    setFilterNumber,
   };
 
   return (
